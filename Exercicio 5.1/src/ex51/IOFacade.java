bd6ccab44ec33695b83d605f962c3ba0df0245d0
Lusccao <lucasarthur7@gmail.com>
2020-04-22 16:00:55 -0300

    Correção e melhoria ex 5.1

--- Exercicio 5.1/src/ex51/IOFacade.java
+++ Exercicio 5.1/src/ex51/IOFacade.java
@@ -3,12 +3,13 @@ package ex51;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.file.Files;
 import java.util.Scanner;
 
 public class IOFacade {
+
 	public String abrirTexto(String arquivo) {
 		StringBuilder sb = new StringBuilder();
 		try (Scanner scanner = new Scanner(new File(arquivo))) {
@@ -16,32 +17,30 @@ public class IOFacade {
 				sb.append(scanner.nextLine());
 			}
 		} catch (FileNotFoundException e) {
-			System.out.println("N�o achei o arquivo pra ler");
+			System.out.println("Não achei o arquivo pra ler");
 			e.printStackTrace();
 		}
 		return sb.toString();
 	}
-	public Byte[] abrirBinario(String arquivo) {
-		List<Byte> listaBytes = new ArrayList<>();
-		try (Scanner scanner = new Scanner(new File(arquivo))) {
-			while (scanner.hasNextByte()) {
-				listaBytes.add(scanner.nextByte());
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("N�o achei o arquivo pra ler");
-			e.printStackTrace();
-		}
-		return (Byte[]) listaBytes.toArray();
-	}
-
-	public Object abrirObjeto(String arquivo) {
-		try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(arquivo))) {
-			return in.readObject();
-		} catch (Exception ex) {
-			ex.printStackTrace();
+	public byte[] abrirBinario(String arquivo) {
+		try {
+			byte[] fileContent = Files.readAllBytes(new File(arquivo).toPath());
+			return fileContent;
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
 		return null;
 	}
+
+	public Object abrirObjeto(String arquivo) {
+		try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(arquivo))) {;
+		return in.readObject();
+	} catch (Exception ex) {
+		ex.printStackTrace();
+	}
+	return null;
+}
+
 }
 
 
